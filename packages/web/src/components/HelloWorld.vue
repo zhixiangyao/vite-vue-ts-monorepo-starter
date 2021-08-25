20ea4af0b573144556dfe26cbbf66c9d2d7c7a56
zhixiangyao <blueskyzx@icloud.com>
2021-08-25 12:50:39 +0800

    fix

--- packages/web/src/components/HelloWorld.vue
+++ packages/web/src/components/HelloWorld.vue
@@ -7,13 +7,13 @@ export default defineComponent({ name: 'HelloWorld' })
 <script lang="ts" setup>
 import { ref, defineProps } from 'vue'
 
-defineProps<{ msg: string }>()
+const props = defineProps<{ msg: string }>()
 
 let count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
 </template>
